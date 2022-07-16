e5a8bea38f46d3c0de0c8eefb0f8f8ee042a4ef8
Pepijn de Vos <pepijndevos@gmail.com>
2022-07-16 20:23:23 +0200

    Add files via upload

---
+++ tilemap/village tileset 16x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="village tileset 16x" tilewidth="16" tileheight="16" tilecount="160" columns="16">
+ <image source="village tileset.png" width="256" height="160"/>
+</tileset>
---
+++ tilemap/village tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="village tileset" tilewidth="8" tileheight="8" tilecount="640" columns="32">
+ <image source="village tileset.png" width="256" height="160"/>
+</tileset>
